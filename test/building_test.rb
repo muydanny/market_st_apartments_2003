7106e04b1d86efd19a02a904bdce46f97c132b95
Danny Ramos <danramos21@gmail.com>
2020-03-31 10:51:59 -0600

    Added more tests

--- test/building_test.rb
+++ test/building_test.rb
@@ -29,24 +29,24 @@ class BuildingTest < Minitest::Test
     assert_equal [], building.renters
   end
 
-  # def test_building_can_add_renters
-  #   building = Building.new
-  #   unit1 = Apartment.new({number: "A1", monthly_rent: 1200, bathrooms: 1, bedrooms: 1})
-  #   renter1 = Renter.new("Aurora")
-  #   unit1.add_renter(renter1)
-  #   assert_equal ["Aurora"], building.renters
-  #   unit2.add_renter(renter2)
-  #   unit2 = Apartment.new({number: "B2", monthly_rent: 999, bathrooms: 2, bedrooms: 2})
-  #   assert_equal ["Aurora", "Tim"], building.renters
-  # end
-  #
-  # def test_it_has_average_rent
-  #   building = Building.new
-  #   unit1 = Apartment.new({number: "A1", monthly_rent: 1200, bathrooms: 1, bedrooms: 1})
-  #   unit2 = Apartment.new({number: "B2", monthly_rent: 999, bathrooms: 2, bedrooms: 2})
-  #
-  #   assert_equal 1099.5, building.average_rent
-  # end
+  def test_building_can_add_renters
+    building = Building.new
+    unit1 = Apartment.new({number: "A1", monthly_rent: 1200, bathrooms: 1, bedrooms: 1})
+    renter1 = Renter.new("Aurora")
+    unit1.add_renter(renter1)
+    assert_equal ["Aurora"], building.renters
+    unit2.add_renter(renter2)
+    unit2 = Apartment.new({number: "B2", monthly_rent: 999, bathrooms: 2, bedrooms: 2})
+    assert_equal ["Aurora", "Tim"], building.renters
+  end
+
+  def test_it_has_average_rent
+    building = Building.new
+    unit1 = Apartment.new({number: "A1", monthly_rent: 1200, bathrooms: 1, bedrooms: 1})
+    unit2 = Apartment.new({number: "B2", monthly_rent: 999, bathrooms: 2, bedrooms: 2})
+
+    assert_equal 1099.5, building.average_rent
+  end
 
   def test_it_has_rented_units
     building = Building.new
@@ -59,5 +59,26 @@ class BuildingTest < Minitest::Test
     renter1 = Renter.new("Spencer")
     unit2.add_renter(renter1)
     assert_equal [unit2, renter1], building.rented_units
+  end
+
+  def test_it_has_the_highest_rent
+    building = Building.new
+    unit1 = Apartment.new({number: "A1", monthly_rent: 1200, bathrooms: 1, bedrooms: 1})
+    unit2 = Apartment.new({number: "B2", monthly_rent: 999, bathrooms: 1, bedrooms: 2})
+    unit3 = Apartment.new({number: "C3", monthly_rent: 1150, bathrooms: 2, bedrooms: 2})
+    renter1 = Renter.new("Spencer")
+    renter2 = Renter.new("Jessie")
+    unit1.add_renter(renter2)
+    renter3 = Renter.new("Max")
+    assert_equal
+  end
+
+  def test_units_by_number_of_bedrooms
+    building = Building.new
+    unit1 = Apartment.new({number: "A1", monthly_rent: 1200, bathrooms: 1, bedrooms: 1})
+    unit2 = Apartment.new({number: "B2", monthly_rent: 999, bathrooms: 1, bedrooms: 2})
+    unit3 = Apartment.new({number: "C3", monthly_rent: 1150, bathrooms: 2, bedrooms: 2})
+    unit4 = Apartment.new({number: "D4", monthly_rent: 1500, bathrooms: 2, bedrooms: 3})
+    assert_equal {3 => ["D4" ], 2 => ["B2", "C3"], 1 => ["A1"]}, building.units_by_number_of_bedrooms
   end
 end
